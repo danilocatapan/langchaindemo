cdcc9e81d1688ed6367ff198e0a651b35f50a59b
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2024-05-04 10:37:39 -0300

    feat: adicionado o endpoint /answerModel utilizando OpenAiChatModel.OpenAiChatModelBuilder

--- src/main/java/com/catapan/openia/resource/OpenAIResource.java
+++ src/main/java/com/catapan/openia/resource/OpenAIResource.java
@@ -2,6 +2,7 @@ package com.catapan.openia.resource;
 
 import com.catapan.openia.dto.MyQuestion;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -9,23 +10,44 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
-@Path("/answer")
+@Produces(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.APPLICATION_JSON)
 public class OpenAIResource {
 
     private static final Logger LOGGER = Logger.getLogger(OpenAIResource.class);
+
+    @ConfigProperty(name = "quarkus.langchain4j.openai.api-key")
+    String apiKey;
 
     @Inject
     private ChatLanguageModel chatModel;
 
     @POST
-    @Produces(MediaType.TEXT_PLAIN)
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/answer")
     public Response chatWithOpenAI(MyQuestion question) {
         LOGGER.info("Recebido: " + question.question());
+
         String answer = chatModel.generate(question.question());
+
         LOGGER.info("Resposta: " + answer);
         return Response.ok(answer).build();
     }
+
+    @POST
+    @Path("/answerModel")
+    public Response chatModelWithOpenAI(MyQuestion question) {
+        LOGGER.info("Recebido: " + question.question());
+
+        ChatLanguageModel customModel = new OpenAiChatModel.OpenAiChatModelBuilder()
+                .apiKey(apiKey)
+                .modelName("gpt-3.5-turbo")
+                .temperature(0.1).build();
+
+        LOGGER.info("Resposta: " + customModel);
+        return Response.ok(customModel.generate(question.question())).build();
+    }
+
 }
