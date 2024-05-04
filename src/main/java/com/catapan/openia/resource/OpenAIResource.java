b3b5cfa1a46732c2cb5b8ef84d4342aafc96669c
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2024-05-04 10:45:31 -0300

    feat: adicionado endpoint /receita para o uso da MyStructuredTemplate

--- src/main/java/com/catapan/openia/resource/OpenAIResource.java
+++ src/main/java/com/catapan/openia/resource/OpenAIResource.java
@@ -1,10 +1,15 @@
 package com.catapan.openia.resource;
 
 import com.catapan.openia.dto.MyQuestion;
+import com.catapan.openia.dto.MyStructuredTemplate;
+import com.catapan.openia.dto.MyStructuredTemplate.PromptDeReceita;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.structured.StructuredPromptProcessor;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -12,6 +17,8 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
+
+import java.util.Arrays;
 
 @Produces(MediaType.TEXT_PLAIN)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -37,7 +44,7 @@ public class OpenAIResource {
     }
 
     @POST
-    @Path("/answerModel")
+    @Path("/answer/model")
     public Response chatModelWithOpenAI(MyQuestion question) {
         LOGGER.info("Recebido: " + question.question());
 
@@ -50,4 +57,18 @@ public class OpenAIResource {
         return Response.ok(customModel.generate(question.question())).build();
     }
 
+    @GET
+    @Path("/receita")
+    public String crieReceita() {
+        @java.lang.SuppressWarnings("java:S1481")
+        MyStructuredTemplate template = new MyStructuredTemplate();
+
+        PromptDeReceita promptDeReceita = new PromptDeReceita();
+        promptDeReceita.prato = "Assado";
+        promptDeReceita.ingredientes = Arrays.asList("carne", "tomate", "cebola", "pimentao");
+
+        Prompt prompt = StructuredPromptProcessor.toPrompt(promptDeReceita);
+
+        return chatModel.generate(prompt.text());
+    }
 }
